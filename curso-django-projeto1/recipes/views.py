25c23f8c136e8c935bd54cd1a655aa37caf87d02
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-09-13 16:31:00 -0300

    entendendo templates 2

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name' : 'Luccas Castro',
+    })
 
 def contato(request):
     return HttpResponse('CONTATO')
