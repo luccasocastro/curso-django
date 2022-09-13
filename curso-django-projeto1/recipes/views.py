09705ccd45ebd45462b86134b484339a31d23a6c
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-09-13 16:14:34 -0300

    entendendo um pouco sobre templates

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO')
