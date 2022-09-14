02a4a18cf779c582b73c948400bc45bab122cde4
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-09-13 23:49:27 -0300

    primeiros componentes do site

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name' : 'Luccas Castro',
     })
 
