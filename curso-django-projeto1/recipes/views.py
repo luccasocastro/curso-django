55602fa0e74912efd443b9a1e7f70702d7d9e242
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-09-21 16:38:44 -0300

    algumas alterações

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -7,3 +7,8 @@ def home(request):
         'name' : 'Luccas Castro',
     })
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name' : 'Luccas Castro',
+    })
+
