43cbdc26b6aa4386157a042447c7e78c5427e992
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-09-25 18:26:41 -0300

    API books

--- library/library/urls.py
+++ library/library/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from books.api import viewsets as bookviewsets
+from django.conf.urls.static import static
+from django.conf import settings
 
 route = routers.DefaultRouter()
 route.register(r'books', bookviewsets.BooksViewSet, basename='Books')
@@ -24,4 +26,4 @@ route.register(r'books', bookviewsets.BooksViewSet, basename='Books')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
