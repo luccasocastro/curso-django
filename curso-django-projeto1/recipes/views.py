093a29645732a1de72728c1c4bc77b408853561f
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-11-11 01:47:53 -0300

    dinamizando os dados das recipes

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name' : 'Luccas Castro',
+        'recipes' : [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name' : 'Luccas Castro',
+        'recipe' : make_recipe(),
     })
 
